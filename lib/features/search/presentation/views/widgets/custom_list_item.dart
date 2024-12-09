5e6840922ddeeb8c8011d067a86339a0ea898964
ibrahim elgebaly <ibrahimelgebaly26@gmail.com>
2024-12-09 12:13:03 +0200

    ui search screen

---
+++ lib/features/search/presentation/views/widgets/custom_list_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomListItem extends StatelessWidget {
+  const CustomListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxlN43fZveSjOFapAiHF6jEqWmQRWA273LTw&s",
+        width: 140.w,
+        height: 89.h,
+        fit: BoxFit.cover,
+      ),
+      title: Text("Harry Potter Pack"),
+      subtitle: Text("2001\nThe Philosopher`s Stone"),
+    );
+  }
+}
